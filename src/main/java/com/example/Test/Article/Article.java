eaf4ff28beb881cf82e886c69d37ae8db64b962c
oourora <hyun_aaaa@naver.com>
2023-07-10 04:15:03 +0900

    mysql 연결완료..

--- src/main/java/com/example/Test/Article/Article.java
+++ src/main/java/com/example/Test/Article/Article.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Setter
--- src/main/java/com/example/Test/Article/ArticleRepository.java
+++ src/main/java/com/example/Test/Article/ArticleRepository.java
@@ -3,8 +3,8 @@ package com.example.Test.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
-
-    Article findBySubject(String subject);
-    Article findBySubjectAndContent(String subject, String content);
+//
+//    Article findBySubject(String subject);
+//    Article findBySubjectAndContent(String subject, String content);
 
 }
