59b4c009486432aa913296de4a4ead5c21de2775
hyun_aaaa <hyun_aaaa@naver.com>
2023-07-18 23:08:32 +0900

    수정 + 삭제 // 구현 완료

--- src/main/java/com/example/Test/Application.java
+++ src/main/java/com/example/Test/Application.java
@@ -2,7 +2,10 @@ package com.example.Test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class Application {
 
@@ -10,10 +13,10 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 
 
-
-
-
 	}
-
+	@GetMapping("/")
+	public String root() {
+		return "redirect:/article/list";
+	}
 
 }
